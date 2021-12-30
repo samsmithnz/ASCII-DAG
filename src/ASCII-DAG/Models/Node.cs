3efcec7714dddcb575f7beb238a6f3b99ff1e07e
Sam Smith <samsmithnz@gmail.com>
2021-12-30 18:25:35 -0500

    Added basic model

---
+++ src/ASCII-DAG/Models/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace ASCII_DAG.Models
+{
+    public class Node
+    {
+        public Node()
+        {
+            ItemPrereqs = new();
+        }
+
+        public string? Name { get; set; }
+        public List<Node> ItemPrereqs { get; set; }
+    }
+}
