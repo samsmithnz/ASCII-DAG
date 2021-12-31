1738718be8db15e7827ae8d2fcbb9abf7d8d232c
Sam Smith <samsmithnz@gmail.com>
2021-12-30 21:33:03 -0500

    Added test for multiple nodes

--- src/ASCII-DAG.Tests/NodeTests.cs
+++ src/ASCII-DAG.Tests/NodeTests.cs
@@ -19,4 +19,30 @@ public class NodeTests
         Assert.IsNull(node.Name);
         Assert.AreEqual(0, node.NodePrereqs.Count);
     }
+
+    [TestMethod]
+    public void NodesAToBTest()
+    {
+        //Arrange
+        Node nodeA = new()
+        {
+            Name = "A"
+        };
+        Node nodeB = new()
+        {
+            Name = "B",
+            NodePrereqs = new() { nodeA }
+        };
+
+
+        //Act
+
+        //Assert
+        Assert.IsNotNull(nodeA);
+        Assert.IsNotNull(nodeB);
+        Assert.AreEqual("A",nodeA.Name);
+        Assert.AreEqual("B",nodeB.Name);
+        Assert.AreEqual(0, nodeA.NodePrereqs.Count);
+        Assert.AreEqual(1, nodeB.NodePrereqs.Count);
+    }
 }
